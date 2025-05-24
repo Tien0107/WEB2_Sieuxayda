454809ead09c78b837867116e569d245081468e3
phamdongchinh683 <phamdongchinh123789@gmail.com>
2025-05-24 10:10:49 +0700

    update show list 24-5-2025

--- homework2/src/main/java/com/example/homework2/controller/MemberController.java
+++ homework2/src/main/java/com/example/homework2/controller/MemberController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.homework2.model.Member;
@@ -18,6 +17,7 @@ public class MemberController {
 
     @GetMapping("/members")
     public String showAddMemberForm(Model model) {
+        model.addAttribute("members", memberRepository.findAll());
         model.addAttribute("member", new Member());
         return "add-member";
     }
@@ -29,11 +29,4 @@ public class MemberController {
         return "add-member";
     }
 
-    @GetMapping("/members/:id")
-    public String showMemberDetails(@PathVariable Long id, Model model) {
-        Member member = memberRepository.findById(id).orElse(null);
-        model.addAttribute("member", member);
-        return "member-details";
-    }
-
-} 
+}
