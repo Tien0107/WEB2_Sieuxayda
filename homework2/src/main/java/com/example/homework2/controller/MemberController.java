3a92713a326d1e0e674b9e01251d45ac2cfacf97
nguyen tien0107 <tien100609@donga.edu.vn>
2025-05-24 11:17:11 +0700

    update h2 config

--- homework2/src/main/java/com/example/homework2/controller/MemberController.java
+++ homework2/src/main/java/com/example/homework2/controller/MemberController.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+// import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.homework2.model.Member;
@@ -18,6 +18,7 @@ public class MemberController {
 
     @GetMapping("/members")
     public String showAddMemberForm(Model model) {
+        model.addAttribute("members", memberRepository.findAll());
         model.addAttribute("member", new Member());
         return "add-member";
     }
@@ -29,11 +30,11 @@ public class MemberController {
         return "add-member";
     }
 
-    @GetMapping("/members/:id")
-    public String showMemberDetails(@PathVariable Long id, Model model) {
-        Member member = memberRepository.findById(id).orElse(null);
-        model.addAttribute("member", member);
-        return "member-details";
-    }
+    // @GetMapping("/members/:id")
+    // public String showMemberDetails(@PathVariable Long id, Model model) {
+    //     Member member = memberRepository.findById(id).orElse(null);
+    //     model.addAttribute("member", member);
+    //     return "member-details";
+    // }
 
 } 
