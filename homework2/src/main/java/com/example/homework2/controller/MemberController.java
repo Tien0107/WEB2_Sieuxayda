c1c238579c2d33e0316deff8e48a660899eebf39
nguyen tien0107 <tien100609@donga.edu.vn>
2025-05-24 11:25:46 +0700

    update h2 nonstop

--- homework2/src/main/java/com/example/homework2/controller/MemberController.java
+++ homework2/src/main/java/com/example/homework2/controller/MemberController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.homework2.model.Member;
@@ -30,11 +29,5 @@ public class MemberController {
         return "add-member";
     }
 
-    // @GetMapping("/members/:id")
-    // public String showMemberDetails(@PathVariable Long id, Model model) {
-    //     Member member = memberRepository.findById(id).orElse(null);
-    //     model.addAttribute("member", member);
-    //     return "member-details";
-    // }
 
 } 
