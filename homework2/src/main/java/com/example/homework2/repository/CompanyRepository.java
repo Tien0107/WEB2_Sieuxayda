c099325dc5303d0f87fcb383bc5a405b671e61d6
nguyen tien0107 <tien100609@donga.edu.vn>
2025-09-12 22:17:09 +0700

    delete

--- homework2/src/main/java/com/example/homework2/repository/CompanyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.homework2.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.homework2.model.Company;
-
-public interface CompanyRepository extends JpaRepository<Company, Long> {
-
-}
--- homework2/src/main/java/com/example/homework2/repository/StaffRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.homework2.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.homework2.model.Staff;
-
-public interface StaffRepository extends JpaRepository<Staff, Long> {
- 
-}
